5f277fb49115bc4f10f422783a517611547df163
TT2TER <93923763+TT2TER@users.noreply.github.com>
2021-11-15 17:27:06 +0800

    update lexue1115榜二！

--- lexue/50.c
+++ lexue/50.c
@@ -1,11 +1,69 @@
-#include<stdio.h>
-#define wl wordlist
-int start[10000];
-int end[10000];
+#include <stdio.h>
+#include <ctype.h>
+char temp[81];
+char dic[50000];
+int posstart[5000];
+int len[5000];
+int compare(int cnt,int l)
+{
+    int ret=0;
+    for(int i=1;i<=cnt;i++)
+    {
+        if(l==len[i])
+        {
+            for(int k=1,j=posstart[i];k<=l;k++,j++)
+            {
+                if(temp[k]==dic[j])ret=i;
+                else 
+                {
+                    ret=0;
+                    break;
+                }
+            }
+            if(ret)break;
+        }
+    }
+    return ret;
+}
 int main()
 {
-    while(getchar()!=EOF)
-    // int *p=new int (10);//new 是动态变量的标志 动态变量没有变量名 
-    // delete p;//杀死动态变量
+    //freopen("1.out","w",stdout);
+    int cnt = 0;
+    int i = 1;
+    len[0] = 0;
+    posstart[0] = 1;
+    while ((temp[i] = getchar())!=EOF)
+    {
+        if (!isalpha(temp[i]) && !isalpha(temp[i - 1]))
+            printf("%c", temp[i]), i = 0;
+        if (!isalpha(temp[i]) && isalpha(temp[i - 1]))
+        {
+            int a = compare(cnt,i-1);
+            if(a)
+            {
+                printf("%d",a);
+                printf("%c", temp[i]);
+                i=0;
+            }
+            if (!a)
+            {
+                cnt++;
+                len[cnt] = i - 1;
+                posstart[cnt] = len[cnt - 1] + posstart[cnt - 1];
+                for (int k = posstart[cnt], j = 1; k < len[cnt] + posstart[cnt]; k++, j++)
+                {
+                    dic[k] = temp[j];
+                }
+
+                for (int k = 1; k < i; k++)
+                {
+                    printf("%c", temp[k]);
+                }
+                printf("%c", temp[i]);
+                i = 0;
+            }
+        }
+        i++;
+    }
     return 0;
 }
